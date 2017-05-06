c037338fa3b6d0eeee732d35354fa725d12cf9a8
olli0601 <oliver.ratmann@ic.ac.uk>
2017-05-06 07:36:03 +0100

    prj update

--- misc/phyloscan.projects.R
+++ misc/phyloscan.projects.R
@@ -12,10 +12,10 @@ project.dual<- function()
 	#pty.pipeline.phyloscanner.160915.couples.rerun()
 	#pty.pipeline.phyloscanner.170301.firstbatchofall()
 	#pty.pipeline.phyloscanner.170301.firstbatchsecondbatchofall.rerun()
-	pty.pipeline.phyloscanner.170301.secondstage() 
+	#pty.pipeline.phyloscanner.170301.secondstage() 
 	#pty.pipeline.phyloscanner.170301.secondstage.ptyr1()	
 	#pty.pipeline.phyloscanner.170301.firstbatchsecondbatchofall.fix()
-	#pty.pipeline.phyloscanner.170301.secondstage.ptyrtrees()
+	pty.pipeline.phyloscanner.170301.secondstage.ptyrtrees()
 	#pty.pipeline.phyloscanner.170301.secondbatchofall()
 	#project.RakaiAll.setup.RAxMLmodel.170301()
 	#pty.pipeline.compress.phyloscanner.output()
@@ -3349,11 +3349,11 @@ pty.pipeline.phyloscanner.170301.secondstage.ptyrtrees<- function()
 	
 	#HOME				<<- '/Users/Oliver/Dropbox (Infectious Disease)/2015_PANGEA_DualPairsFromFastQIVA'	
 	hpc.load			<- "module load intel-suite/2015.1 mpi raxml/8.2.9"
-	if(0)	#first lightweight run to handle most read alignments
+	if(1)	#first lightweight run to handle most read alignments
 	{
 		hpc.select<- 1; hpc.nproc<- 1; 	hpc.walltime<- 3; hpc.mem<- "1850mb"; hpc.q<- NA
 	}
-	if(1)	#second midweight run to handle the remaining read alignments
+	if(0)	#second midweight run to handle the remaining read alignments
 	{
 		hpc.select<- 1; hpc.nproc<- 1; 	hpc.walltime<- 571; hpc.mem<- "3600mb"; hpc.q<- "pqeph"
 		#hpc.select<- 1; hpc.nproc<- 1; 	hpc.walltime<- 71; hpc.mem<- "1800mb"; hpc.q<- NA
@@ -3380,9 +3380,9 @@ pty.pipeline.phyloscanner.170301.secondstage.ptyrtrees<- function()
 	infiles	<- merge(infiles, tmp, by=c('PTY_RUN','W_FROM'), all.x=1)
 	infiles	<- subset(infiles, is.na(FT))	
 	setkey(infiles, PTY_RUN, W_FROM)	
-	
+	 
 	print(infiles)
-	infiles	<- infiles[1:100,]
+	#infiles	<- infiles[1:100,]
 	#stop()
 	df		<- infiles[, list(CMD=cmd.raxml(FI, outfile=FO, pr=raxml.pr, pr.args=raxml.args)), by=c('PTY_RUN','W_FROM')]
 	#df[1, cat(CMD)]	
