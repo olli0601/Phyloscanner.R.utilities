874355150c6c548a2b20c96c8db40d879a3fc2f8
olli0601 <oliver.ratmann@ic.ac.uk>
2017-05-12 15:31:16 +0100

    prj update

--- misc/phyloscan.projects.R
+++ misc/phyloscan.projects.R
@@ -3411,7 +3411,7 @@ pty.pipeline.phyloscanner.170301.secondstage.ptyrtrees<- function()
 		}
 		if(1)	#third heavyweight run to handle the remaining read alignments
 		{
-			hpc.select<- 1; hpc.nproc<- 4; 	hpc.walltime<- 71; hpc.mem<- "7850mb"; hpc.q<- NA
+			hpc.select<- 1; hpc.nproc<- 8; 	hpc.walltime<- 71; hpc.mem<- "7850mb"; hpc.q<- NA
 		}
 		
 		raxml.pr			<- ifelse(hpc.nproc==1, 'raxmlHPC-SSE3', 'raxmlHPC-PTHREADS-SSE3')	
@@ -3441,8 +3441,9 @@ pty.pipeline.phyloscanner.170301.secondstage.ptyrtrees<- function()
 		#infiles	<- subset(infiles, PTY_RUN%in%c(162, 181, 182, 183, 190, 192, 193, 194, 195, 196))
 		#infiles	<- subset(infiles, PTY_RUN%in%c(197, 202, 206, 207, 208, 209, 211, 212, 218, 220))		
 		#infiles	<- subset(infiles, PTY_RUN%in%c(222, 223, 224, 225, 226, 227, 228, 229, 231, 233, 234, 235, 236, 237, 238, 239))
-		infiles		<- subset(infiles, PTY_RUN%in%c(105))
-		#infiles		<- subset(infiles, PTY_RUN%in%c(112, 116, 11, 120, 122, 123, 130, 132, 134, 143, 149, 14, 166, 177, 178, 17, 182, 184, 185, 197, 200, 201, 207, 20, 210, 215, 21, 25, 27, 33, 34, 42, 45, 48, 4, 58, 69, 71, 76, 78, 79, 80, 85, 86, 96))
+		#infiles		<- subset(infiles, PTY_RUN%in%c(105))
+		infiles		<- subset(infiles, PTY_RUN%in%c(112))
+		#infiles		<- subset(infiles, PTY_RUN%in%c(116, 11, 120, 122, 123, 130, 132, 134, 143, 149, 14, 166, 177, 178, 17, 182, 184, 185, 197, 200, 201, 207, 20, 210, 215, 21, 25, 27, 33, 34, 42, 45, 48, 4, 58, 69, 71, 76, 78, 79, 80, 85, 86, 96))
 		print(infiles)	
 		df		<- infiles[, list(CMD=cmd.raxml(FI, outfile=FO, pr=raxml.pr, pr.args=raxml.args)), by=c('PTY_RUN','W_FROM')]
 		#df[1, cat(CMD)]	
@@ -3458,7 +3459,7 @@ pty.pipeline.phyloscanner.170301.secondstage.ptyrtrees<- function()
 	#
 	#	combine all the data
 	#	
-	if(1)
+	if(0)
 	{
 		indirs 	<- '/Users/Oliver/duke/tmp/ptyr143_trees'
 		indirs	<- '/work/or105/Gates_2014/2015_PANGEA_DualPairsFromFastQIVA/RakaiAll_output_170301_w250_s20_p35_stagetwo'
