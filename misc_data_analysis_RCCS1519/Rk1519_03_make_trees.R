2bf5d227a703fa7038c5c60466427228b40ee9d6
Oliver Ratmann <monsieur.olli@gmail.com>
2021-01-22 00:32:20 +0000

    updates and fixups to make_trees.R

--- misc_data_analysis_RCCS1519/Rk1519_03_make_trees.R
+++ misc_data_analysis_RCCS1519/Rk1519_03_make_trees.R
@@ -33,9 +33,11 @@ rkuvri.make.anonymised.id <- function(){
 
 rkuvri.make.phyloscanner.input.samples <- function()
 {
-  #' input person ids, selected samples, ananymise ids
-  #' returns a data table summarising person id and bam files 
-  #' 
+  	#' input person ids, selected samples, ananymise ids
+  	#' returns a data table summarising person id and bam files 
+  	#' 
+  	require(data.table)
+  
 	# file names
 	data.dir <- '/rds/general/project/ratmann_pangea_deepsequencedata/live/'
 	out.base <- '/rds/general/project/ratmann_deepseq_analyses/live/PANGEA2_RCCS1519_UVRI/210120_RCCSUVRI_'
@@ -105,18 +107,23 @@ rkuvri.make.phyloscanner.input.samples <- function()
 
 rkuvri.make.phyloscanner.input.runs <- function()
 {
-  #' input clusters, couples, and three closest persons for each cluster
-  #' return a data table allocating each person to a run
+  	#' input clusters, couples, and three closest persons for each cluster
+  	#' return a data table allocating each person to a run
   
-  data.dir <- '/rds/general/project/ratmann_pangea_deepsequencedata/live/'
-  out.base <- '/rds/general/project/ratmann_deepseq_analyses/live/PANGEA2_RCCS1519_UVRI/210120_RCCSUVRI_'
+	require(data.table)
+	require(tidyverse)
+	
+  	data.dir <- '/rds/general/project/ratmann_pangea_deepsequencedata/live/'
+  	out.base <- '/rds/general/project/ratmann_deepseq_analyses/live/PANGEA2_RCCS1519_UVRI/210120_RCCSUVRI_'	
 	potential.networks.analysis.dir <- "/rds/general/project/ratmann_deepseq_analyses/live/PANGEA2_RCCS1519_UVRI/200929_SIMILARTY_windowsize500_batchsize100"
 	infile.potential.networks <- file.path(potential.networks.analysis.dir, 'clusters.rda')
 	infile.clostest.three.per.cluster <- file.path(potential.networks.analysis.dir, 'closest_3_percluster.rds')
 	infile.couple <- '/rds/general/project/ratmann_deepseq_analyses/live/PANGEA2_RCCS1519_UVRI/RakaiPangeaMetaData_v2.rda'
 	
+	# load data on individuals, sample locations, and rename ids 
+	pty.runs <- readRDS(paste0(out.base,'phscinput_samples.rds'))
 	
-  # load clusters of close pairs and redefine IDCLU
+  	# load potential transmission networks of close pairs and redefine IDCLU
 	load( infile.potential.networks )  
 	tmp <- unique(subset(df,select=c('CLU_SIZE','IDCLU')))
 	setkey(tmp, CLU_SIZE)
@@ -125,102 +132,127 @@ rkuvri.make.phyloscanner.input.runs <- function()
 	rtc	<- subset( merge(df, tmp, by='IDCLU'),select=c('ID','IDCLU2','CLU_SIZE'))
 	setnames(rtc,'IDCLU2','IDCLU')
 	setkey(rtc, IDCLU)
-	cat(max(rtc$IDCLU),' clusters of ',length(unique(rtc$ID)), ' individuals, the ten largest cluster sizes are ', 
+	cat(max(rtc$IDCLU),' potential transmission networks of ',length(unique(rtc$ID)), ' individuals, the ten largest cluster sizes are ', 
 			unique(subset(rtc,select=c('CLU_SIZE','IDCLU')))$CLU_SIZE[(max(rtc$IDCLU)-10+1):max(rtc$IDCLU)], ', ', nrow(rtc)-length(unique(rtc$ID)),
 			'individuals appear in more than one clusters','\n')
-  # 808  clusters of  2364  individuals, the ten largest cluster sizes are  17 18 21 22 26 37 38 45 50 55 ,  150 individuals appear in more than one clusters 
+  	# 808  potential transmission networks of  2364  individuals, the ten largest cluster sizes are  17 18 21 22 26 37 38 45 50 55 ,  150 individuals appear in more than one clusters 
 	
-
+	#
 	#	add couples that are not a close pair and redefine IDCLU
-	load(infile.couple)
-	pty.runs <- readRDS(paste0(out.base,'phscinput_samples.rds'))
-	rp = data.table(unique(subset(coupdat,select=c('male.RCCS_studyid', 'female.RCCS_studyid'))))
+	#
+	
+	#	find which couples are in potential transmission networks
+	load(infile.couple)	
+	rp <- data.table(unique(subset(coupdat,select=c('male.RCCS_studyid', 'female.RCCS_studyid'))))
 	setnames(rp, c('male.RCCS_studyid', 'female.RCCS_studyid'), c('MALE_RID','FEMALE_RID'))
+	rp <- subset(rp, MALE_RID!=FEMALE_RID)
 	rp[,FEMALE_RID:=paste0('RK-',FEMALE_RID)]
 	rp[,MALE_RID:=paste0('RK-',MALE_RID)]
-	tmp<- sort(unique(as.character(pty.runs[['UNIT_ID']])))	
-	rp<- unique(subset(rp, FEMALE_RID%in%tmp & MALE_RID%in%tmp, select=c(FEMALE_RID,MALE_RID)))
-  # rp[FEMALE_RID==MALE_RID]
-	rp<- rp[FEMALE_RID!=MALE_RID] # not sure why
-	setnames(rp, 'FEMALE_RID', 'ID')
-	rp<- merge(rp, subset(rtc, select=c(ID, IDCLU)), all.x=1, by='ID')
+	tmp <- sort(unique(as.character(pty.runs[['UNIT_ID']])))	
+	rp <- unique(subset(rp, FEMALE_RID%in%tmp & MALE_RID%in%tmp, select=c(FEMALE_RID,MALE_RID)))
+  	setnames(rp, 'FEMALE_RID', 'ID')
+	rp <- merge(rp, subset(rtc, select=c(ID, IDCLU)), all.x=1, by='ID')
 	setnames(rp, c('ID','IDCLU','MALE_RID'), c('FEMALE_RID', 'FEMALE_IDCLU','ID'))
-	rp<- merge(rp, subset(rtc, select=c(ID, IDCLU)), all.x=1, by='ID')
+	rp <- merge(rp, subset(rtc, select=c(ID, IDCLU)), all.x=1, by='ID')
 	setnames(rp, c('ID','IDCLU'), c('MALE_RID', 'MALE_IDCLU'))
-	tmp<- rp[, which(is.na(FEMALE_IDCLU) & !is.na(MALE_IDCLU))]
-	set(rp, tmp, 'FEMALE_IDCLU', rp[tmp, MALE_IDCLU])
-	tmp<- rp[, which(!is.na(FEMALE_IDCLU) & is.na(MALE_IDCLU))]
+	rp[, COUP_ID := seq_len(nrow(rp))]
+	
+	#	reassign partners that are not in the same network as their partner
+	tmp <- subset(rp, !is.na(FEMALE_IDCLU) & !is.na(MALE_IDCLU))
+	tmp2 <- tmp[, list(NOT_IN_SAME = !any(FEMALE_IDCLU == MALE_IDCLU)),by=c('MALE_RID','FEMALE_RID')]
+	tmp2 <- subset(tmp2, NOT_IN_SAME)	
+	tmp2 <- merge(tmp2, rp, by=c('MALE_RID','FEMALE_RID'))	
+	tmp <- rp[, which(COUP_ID %in% tmp2$COUP_ID)]
 	set(rp, tmp, 'MALE_IDCLU', rp[tmp, FEMALE_IDCLU])
-	tmp<- rp[, which(is.na(FEMALE_IDCLU) & is.na(MALE_IDCLU))]
+	set(tmp2, NULL, 'FEMALE_IDCLU', tmp2[,MALE_IDCLU])
+	set(tmp2, NULL, 'NOT_IN_SAME', NULL)
+	rp <- rbind(rp, tmp2)
+		
+	#	assign partners that are in no network to the same potential transmission network as their partner
+	tmp <- rp[, which(is.na(FEMALE_IDCLU) & !is.na(MALE_IDCLU))]
+	set(rp, tmp, 'FEMALE_IDCLU', rp[tmp, MALE_IDCLU])
+	tmp <- rp[, which(!is.na(FEMALE_IDCLU) & is.na(MALE_IDCLU))]
+	set(rp, tmp, 'MALE_IDCLU', rp[tmp, FEMALE_IDCLU])
+		
+	#	make a new potential transmission network for couples that are in no network so far
+	tmp <- rp[, which(is.na(FEMALE_IDCLU) & is.na(MALE_IDCLU))]
 	set(rp, tmp, c('FEMALE_IDCLU','MALE_IDCLU'), rtc[, max(IDCLU)]+seq_along(tmp))
+		
+	#	recalculate length of clusters
 	setnames(rp, c('MALE_IDCLU'), c('IDCLU'))
-	rp<- subset(melt(rp, id.vars=c('IDCLU'), measure.vars=c('MALE_RID','FEMALE_RID'), value.name='ID'), select=c(ID, IDCLU))
-	rtc<- unique(rbind(subset(rtc,select=c('ID','IDCLU')), rp))
-	tmp<- rtc[, list(CLU_SIZE=length(ID)), by='IDCLU']
+	rp <- subset(melt(rp, id.vars=c('IDCLU'), measure.vars=c('MALE_RID','FEMALE_RID'), value.name='ID'), select=c(ID, IDCLU))
+	rtc <- unique(rbind(subset(rtc,select=c('ID','IDCLU')), rp))
+	tmp <- rtc[, list(CLU_SIZE=length(ID)), by='IDCLU']
 	setkey(tmp, CLU_SIZE)
 	tmp[, IDCLU2:=seq_len(nrow(tmp))]	
-	rtc<- subset(merge(rtc, tmp, by='IDCLU'),select=c('ID','IDCLU2','CLU_SIZE'))
+	rtc <- subset(merge(rtc, tmp, by='IDCLU'),select=c('ID','IDCLU2','CLU_SIZE'))
 	setnames(rtc, 'IDCLU2', 'IDCLU')
 	setkey(rtc,IDCLU)
-	cat(max(rtc$IDCLU),' clusters of ',length(unique(rtc$ID)), ' individuals, the ten largest cluster sizes are ', 
+	cat(max(rtc$IDCLU),' potential transmission networks of ',length(unique(rtc$ID)), ' individuals, the ten largest cluster sizes are ', 
 			unique(subset(rtc,select=c('CLU_SIZE','IDCLU')))$CLU_SIZE[(max(rtc$IDCLU)-10+1):max(rtc$IDCLU)], ', ', nrow(rtc)-length(unique(rtc$ID)),
 			'individuals appear in more than one clusters','\n')
-  # 1059  clusters of  3032  individuals, the ten largest cluster sizes are  21 22 22 30 31 42 49 50 52 66 ,  242 individuals appear in more than one clusters 
+  	# 1059  potential transmission networks of  3032  individuals, the ten largest cluster sizes are  22 22 23 31 31 43 49 51 52 67 ,  277 individuals appear in more than one clusters 
 	
-  #	merge up to 8 individuals into the same phyloscanner run
+  	#	merge up to 8 individuals into the same phyloscanner run
 	tn	<- 8
 	tmp	<- unique(subset(rtc, select=c(IDCLU, CLU_SIZE)))
 	tmp[, PTY_RUN:= IDCLU]
 	tmp[, PTY_SIZE:= CLU_SIZE]
-  # https://stackoverflow.com/questions/49076769/dplyr-r-cumulative-sum-with-reset
+  	# https://stackoverflow.com/questions/49076769/dplyr-r-cumulative-sum-with-reset
 	sum_reset_at <- function(thresh) {
 		function(x) {
 			accumulate(x, ~if_else(.x+.y>thresh, .y, .x+.y))
 		}  
 	}
-	tmp = tmp %>% mutate(PTY_SIZE2 = sum_reset_at(tn)(PTY_SIZE)) 
-	
-	tmp2 = which(diff(tmp$PTY_SIZE2)<0) + 1
-	tmp$PTY_RUN2 = 0 
-	tmp[c(tmp2,max(tmp2):nrow(tmp)),PTY_RUN2:=1]
+	tmp <- tmp %>% mutate(PTY_SIZE2 = sum_reset_at(tn)(PTY_SIZE))
+	tmp <- as.data.table(tmp)	
+	tmp2 <- which(diff(tmp$PTY_SIZE2)<0) + 1
+	tmp[, PTY_RUN2 := 0]	
+	tmp[c(tmp2,max(tmp2):nrow(tmp)), PTY_RUN2:=1]
 	tmp[,PTY_RUN2:=cumsum(PTY_RUN2)]
 	stopifnot(sum(tmp[,cumsum(PTY_SIZE),by='PTY_RUN2']$V1 !=tmp$PTY_SIZE2)==0)
 	tmp[,PTY_SIZE2:=max(PTY_SIZE2),by='PTY_RUN2']
 	tmp[,PTY_RUN2:=PTY_RUN2+1]
 	set(tmp,NULL,c('PTY_RUN', 'PTY_SIZE'),NULL)
 	setnames(tmp,c('PTY_RUN2', 'PTY_SIZE2'), c('PTY_RUN', 'PTY_SIZE'))
-	rtc = merge(rtc,tmp,by=c('IDCLU', 'CLU_SIZE'))
+	rtc <- merge(rtc,tmp,by=c('IDCLU', 'CLU_SIZE'))
 	
-  #	for all individuals in a cluster
-  #	find 3 closest others
+  	#	for all individuals in a cluster
+  	#	find 3 closest others
 	
-	if(file.exists(infile.clostest.three.per.cluster)){
-	  dcl =readRDS(infile.clostest.three.per.cluster)
-	}else{
+	if(file.exists(infile.clostest.three.per.cluster))
+	{
+	  dcl <- readRDS(infile.clostest.three.per.cluster)
+	}
+	else
+	{
 	  infile.average.distance.per.pair <- file.path(potential.networks.analysis.dir, 'average_distance_perpair.rds')
-	  if(file.exists(infile.average.distance.per.pair)){
-	    ddist = readRDS(infile.average.distance.per.pair)
-	  }else{
+	  if(file.exists(infile.average.distance.per.pair))
+	  {
+	    ddist <- readRDS(infile.average.distance.per.pair)
+	  }
+	  else
+	  {
 	    infile.average.distance.per.pair.per.window <- file.path(potential.networks.analysis.dir, 'distance_overall_method2_sequence_level_v4.rda')
 	    load(infile.average.distance.per.pair.per.window)
-	    ddist = subset(distance,select=c('TAXA1','TAXA2'))
-	    ddist$DIST = rowMeans(distance[,3:ncol(distance)],na.rm = T)
+	    ddist <- subset(distance,select=c('TAXA1','TAXA2'))
+	    ddist$DIST <- rowMeans(distance[,3:ncol(distance)],na.rm = TRUE)
 	    setnames(id.dt, colnames(id.dt),paste0(colnames(id.dt),'1'))
-	    ddist = merge(ddist, id.dt, by.x='TAXA1', by.y='PANGEA_ID1',all.x=T)
+	    ddist <- merge(ddist, id.dt, by.x='TAXA1', by.y='PANGEA_ID1',all.x=TRUE)
 	    setnames(id.dt, colnames(id.dt),gsub('1','2',colnames(id.dt)))
-	    ddist = merge(ddist, id.dt, by.x='TAXA2', by.y='PANGEA_ID2',all.x=T)
+	    ddist <- merge(ddist, id.dt, by.x='TAXA2', by.y='PANGEA_ID2',all.x=TRUE)
 	    setnames(id.dt, colnames(id.dt),gsub('2','',colnames(id.dt)))
 	    ddist <- ddist[,list(DIST=mean(DIST)),by=c('UNIT_ID1','UNIT_ID2')]
 	    saveRDS(ddist,file=infile.average.distance.per.pair) 
 	  }
 	  dcl <- rtc[,{
-	    tmp = ddist[UNIT_ID1 %in% ID, c('UNIT_ID2','DIST')]
-	    tmp2 = ddist[UNIT_ID2 %in% ID, c('UNIT_ID1','DIST')]
-	    tmp = rbind(tmp,tmp2,use.names=F)
-	    colnames(tmp) = c('UNIT_ID','DIST')
-	    tmp = tmp[!UNIT_ID%in% ID,]
-	    tmp = tmp[,list(mean(DIST)),by='UNIT_ID']
-	    tmp=tmp[order(V1),]
+	    tmp <- ddist[UNIT_ID1 %in% ID, c('UNIT_ID2','DIST')]
+	    tmp2 <- ddist[UNIT_ID2 %in% ID, c('UNIT_ID1','DIST')]
+	    tmp <- rbind(tmp,tmp2,use.names=F)
+	    colnames(tmp) <- c('UNIT_ID','DIST')
+	    tmp <- tmp[!UNIT_ID%in% ID,]
+	    tmp <- tmp[,list(mean(DIST)),by='UNIT_ID']
+	    tmp <- tmp[order(V1),]
 	    list(ID_CLOSE1=tmp$UNIT_ID[nrow(tmp)],DISTANCE1=tmp$V1[nrow(tmp)],
 	         ID_CLOSE2=tmp$UNIT_ID[nrow(tmp)-1],DISTANCE2=tmp$V1[nrow(tmp)-1],
 	         ID_CLOSE3=tmp$UNIT_ID[nrow(tmp)-2],DISTANCE3=tmp$V1[nrow(tmp)-2])
@@ -228,9 +260,9 @@ rkuvri.make.phyloscanner.input.runs <- function()
 	  saveRDS(dcl,file=infile.clostest.three.per.cluster)
 	}
 	
-  # add the 3 closest individuals to each cluster
-	tmp = subset(dcl,select=c('CLU_SIZE', 'IDCLU',  'ID_CLOSE1',  'ID_CLOSE2',  'ID_CLOSE3'))
-	tmp = melt(tmp, id.vars=c('CLU_SIZE', 'IDCLU'),variable.name= 'CLOSEST', value.name='ID')
+  	# add the 3 closest individuals to each potential transmission network
+	tmp <- subset(dcl, select=c('CLU_SIZE', 'IDCLU', 'ID_CLOSE1', 'ID_CLOSE2', 'ID_CLOSE3'))
+	tmp <- melt(tmp, id.vars=c('CLU_SIZE', 'IDCLU'), variable.name= 'CLOSEST', value.name='ID')
 	tmp[,CLOSEST:=as.integer(gsub('ID_CLOSE','',CLOSEST))]
 	tmp2 <- unique(subset(rtc,select=c('IDCLU','PTY_RUN')))
 	set(rtc, NULL ,c('PTY_SIZE', 'PTY_RUN'), NULL)
@@ -239,25 +271,25 @@ rkuvri.make.phyloscanner.input.runs <- function()
 	rtc[, ID_TYPE:='target']
 	rtc <- rbind(rtc,tmp)
 	rtc <- unique(rtc)
-	rtc[,CLU_SIZE:=length(ID),by='IDCLU']
+	rtc[, CLU_SIZE:=length(ID),by='IDCLU']
 	rtc <- merge(rtc, tmp2, by='IDCLU')
-	rtc[,PTY_SIZE:=length(ID),by='PTY_RUN']
+	rtc[, PTY_SIZE:=length(ID),by='PTY_RUN']
 	
 	cat(max(rtc$IDCLU),' clusters of ',length(unique(rtc$ID)), ' individuals, the ten largest cluster sizes are ', 
 			unique(subset(rtc,select=c('CLU_SIZE','IDCLU')))$CLU_SIZE[(max(rtc$IDCLU)-10+1):max(rtc$IDCLU)], ', ', nrow(rtc)-length(unique(rtc$ID)),
 			'individuals appear in more than one clusters','\n')
-  # 1059  clusters of  3253  individuals, the ten largest cluster sizes are  24 25 25 33 34 45 52 53 55 69 ,  3096 individuals appear in more than one clusters 
-	tmp = unique(subset(rtc,select=c('PTY_RUN','PTY_SIZE')))
+  	# 1059  clusters of  3253  individuals, the ten largest cluster sizes are  25 25 26 34 34 46 52 54 55 70 ,  3131 individuals appear in more than one clusters 
+	
+	tmp <- unique(subset(rtc,select=c('PTY_RUN','PTY_SIZE')))
 	table(tmp$PTY_SIZE)
-  # 6   8   9  10  11  12  13  14  15  17  19  20  22  23  24  25  33  34  45  52 
-  # 1  38  26  14   7  84   6  38   1  16   1 159   1   1   2   2   1   1   1   1 
-  # 53  55  69 
-  # 1   1   1 
+  	#   6   8   9  10  11  12  13  14  16  17  18  19  20  22  23  24  25  26  34  46
+  	#	1  38  28  15   7  90   5  38   1  15   1   1 156   1   1   1   2   1   2   1
+ 	#	52  54  55  70
+  	#	1   1   1   1
 	
 	# write processed samples 
 	cat('\nWriting to file ', paste0(out.base,'phscinput_runs.rds') )
-	saveRDS(rtc, file=paste0(out.base,'phscinput_runs.rds'))
-		
+	saveRDS(rtc, file=paste0(out.base,'phscinput_runs.rds'))		
 }
 
 
