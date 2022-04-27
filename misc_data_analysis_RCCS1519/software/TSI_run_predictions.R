42f72492529a5f0d6c4466b3766a34a74c75988c
abriz97 <andreabrizzi@live.it>
2022-04-27 10:56:11 +0100

    doc

--- misc_data_analysis_RCCS1519/software/TSI_run_predictions.R
+++ misc_data_analysis_RCCS1519/software/TSI_run_predictions.R
@@ -154,6 +154,100 @@ get.sampling.dates <- function(phsc.samples = args$phsc.samples)
         return(ddates)
 }
 
+write.mafs.and.cmds <-function(pty_idx)
+{
+        # For each PTY index, computes the MAF matrix and stores it in the phscrel directory
+        # Also, writes bash command to set up HIV-phylo-TSI
+
+        cat('Processing PTY index: ', pty_idx, '...\n')
+
+        # Load patstats
+        files_pty <- as.vector(dfiles[pty==pty_idx]) 
+        patstats <- as.data.table(read.csv(files_pty$pat.path, header=TRUE, sep=",", stringsAsFactors=F))
+        if(any((patstats$xcoord %% 1) != 0)){
+                patstats[, xcoord:=ceiling(xcoord)]
+                write.csv(patstats, files_pty$pat.path)
+        }
+        ph.input <- as.data.table(read.csv(files_pty$phi.path, header=FALSE, sep=",",stringsAsFactors=F))
+        colnames(ph.input) <- c('BAM_PATH','FASTA_PATH', 'SAMPLE_ID')
+        ph.input[, AID:=gsub('-fq.*?$','', SAMPLE_ID)]
+        stopifnot( all(patstats[, unique(host.id)] %in% ph.input[, unique(AID)]) )
+
+        # (Checked that BAM and FASTA files are consistent in terms of namings and locs)
+
+        # Find BAM_PATH then get the MAF
+        ph.input[, HXB2_PATH := gsub('.bam$','_BaseFreqs_WithHXB2.csv', basename(BAM_PATH))]
+        if(user == 'andrea')
+        {
+                fraser.dir <- '~/Documents/Box/ratmann_pangea_deepsequencedata/fraserupload-KoPKvNP7dgmnfnxE/well/fraser/DATA/processing'
+                hxb2files <- as.character(list.files(fraser.dir, recursive=TRUE, pattern='^.*?HXB2.csv', full.names = TRUE))
+                hxb2files <- data.table(FULL=hxb2files, BASE=basename(hxb2files))
+                ph.input <- merge(ph.input, hxb2files, by.x='HXB2_PATH', by.y='BASE', all.x=TRUE)
+                ph.input[, `:=`(HXB2_PATH=FULL, FULL=NULL)]
+                ph.input[, HXB2_EXISTS:=file.exists(HXB2_PATH) ]
+        }else{
+                ph.input[, HXB2_PATH := file.path(dirname(BAM_PATH), HXB2_PATH)]
+                ph.input[, HXB2_EXISTS := file.exists(HXB2_PATH)]
+        }
+        maf <- ph.input[, .(SAMPLE_ID, HXB2_PATH, HXB2_EXISTS)]
+        maf_mat <- generate.sample(maf)
+
+        # If there are multiple sequences associated to one AID:
+        # take sequence with associated BaseFreq file ("HXB2")
+        # with latest collection date  
+        tmp1 <- gsub('-fq.*?$','',rownames(maf_mat))
+        tmp1 <- unique(tmp1[duplicated(tmp1)])
+        tmp1 <- data.table(AID = tmp1)
+        if(tmp1[, .N>1])
+        {
+                tmp1 <- tmp1[, list(FQ=grep(AID, rownames(maf_mat), value=T)),by=AID]
+                # which do not have HXB2?
+                tmp1 <- tmp1[, list(HXB2 = !is.na(maf_mat[FQ, 1])), by=c("AID","FQ") ]
+                tmp1 <- merge(tmp1, ddates, by.x=c('AID', 'FQ'), by.y=c("AID", "SAMPLE_ID"))
+                setorder(ddates, AID, -visit_dt)
+                tmp1 <- tmp1[, {
+                        z <- which(HXB2 == TRUE)[1];
+                        z <- ifelse(is.na(z), 1, z)
+                        list(FQ=FQ[z], visit_dt=visit_dt[z])
+                }, by='AID']
+
+
+                rows_to_del <- rownames(maf_mat)[which(grepl(paste0(tmp1$AID, collapse='|'), rownames(maf_mat) ))]
+                rows_to_del <- rows_to_del[! rows_to_del %in% tmp1$FQ]
+                
+                # Store -fq used so we can check exact sampling date
+                maf_mat <- maf_mat[! rownames(maf_mat) %in% rows_to_del, ]
+        }else{
+                rows_to_del <- c()
+        }
+        filename=paste0('ptyr', pty_idx, '_basefreqs_used.csv')
+
+        write.csv(rownames(maf_mat), 
+                  file=file.path(dirname(files_pty$pat.path), filename))
+
+        rownames(maf_mat) <- gsub('-fq[0-9]$', '', rownames(maf_mat))
+        rm(tmp1, rows_to_del)
+        maf_file=paste0('ptyr', pty_idx, '_maf.csv')
+        maf_file=file.path(dirname(files_pty$pat.path), maf_file)
+        cat(maf_mat[2, 1:10], '\n')
+        write.table(maf_mat, file=maf_file, quote=F, col.names=FALSE, sep=',')
+
+
+        # writing command
+        #________________
+        cmd <- paste0('python ',args$TSI.dir,'/HIVPhyloTSI.py \\\n',
+                      ' -d ', args$TSI.dir, '/Model \\\n',
+                      ' -p ', files_pty$pat.path,' \\\n',
+                      ' -m ', files_pty$maf.path,' \\\n',
+                      ' -o ', files_pty$tsi.path, '\n'
+                     )
+        dfiles[pty==pty_idx, CMD:=cmd]
+
+        # guess I may need to free up some memory here
+        rm(maf_mat, ph.input, patstats)
+        gc()
+}
+
 ###############
 # testing
 ###############
@@ -225,90 +319,10 @@ dfiles[, tsi.path:=gsub('patStats.csv$','tsi.csv',pat.path)]
 dfiles[, CMD:=NA_character_]
 
 
+
+
+lapply(dfiles$pty, write.mafs.and.cmds)
 for (pty_idx in dfiles$pty)
-{
-        cat('Processing PTY index: ', pty_idx, '...\n')
-
-        # Load patstats
-        files_pty <- as.vector(dfiles[pty==pty_idx]) 
-        patstats <- as.data.table(read.csv(files_pty$pat.path, header=TRUE, sep=",", stringsAsFactors=F))
-        if(any((patstats$xcoord %% 1) != 0)){
-                patstats[, xcoord:=ceiling(xcoord)]
-                write.csv(patstats, files_pty$pat.path)
-        }
-        ph.input <- as.data.table(read.csv(files_pty$phi.path, header=FALSE, sep=",",stringsAsFactors=F))
-        colnames(ph.input) <- c('BAM_PATH','FASTA_PATH', 'SAMPLE_ID')
-        ph.input[, AID:=gsub('-fq.*?$','', SAMPLE_ID)]
-        stopifnot( all(patstats[, unique(host.id)] %in% ph.input[, unique(AID)]) )
-
-        # (Checked that BAM and FASTA files are consistent in terms of namings and locs)
-
-        # Find BAM_PATH then get the MAF
-        ph.input[, HXB2_PATH := gsub('.bam$','_BaseFreqs_WithHXB2.csv', basename(BAM_PATH))]
-        if(user == 'andrea')
-        {
-                fraser.dir <- '~/Documents/Box/ratmann_pangea_deepsequencedata/fraserupload-KoPKvNP7dgmnfnxE/well/fraser/DATA/processing'
-                hxb2files <- as.character(list.files(fraser.dir, recursive=TRUE, pattern='^.*?HXB2.csv', full.names = TRUE))
-                hxb2files <- data.table(FULL=hxb2files, BASE=basename(hxb2files))
-                ph.input <- merge(ph.input, hxb2files, by.x='HXB2_PATH', by.y='BASE', all.x=TRUE)
-                ph.input[, `:=`(HXB2_PATH=FULL, FULL=NULL)]
-                ph.input[, HXB2_EXISTS:=file.exists(HXB2_PATH) ]
-        }else{
-                ph.input[, HXB2_PATH := file.path(dirname(BAM_PATH), HXB2_PATH)]
-                ph.input[, HXB2_EXISTS := file.exists(HXB2_PATH)]
-        }
-        maf <- ph.input[, .(SAMPLE_ID, HXB2_PATH, HXB2_EXISTS)]
-        maf_mat <- generate.sample(maf)
-
-        # If there are multiple sequences associated to one AID:
-        # take sequence with associated BaseFreq file ("HXB2")
-        # with latest collection date  
-        tmp1 <- gsub('-fq.*?$','',rownames(maf_mat))
-        tmp1 <- unique(tmp1[duplicated(tmp1)])
-        tmp1 <- data.table(AID = tmp1)
-        if(tmp1[, .N>1])
-        {
-                tmp1 <- tmp1[, list(FQ=grep(AID, rownames(maf_mat), value=T)),by=AID]
-                # which do not have HXB2?
-                tmp1 <- tmp1[, list(HXB2 = !is.na(maf_mat[FQ, 1])), by=c("AID","FQ") ]
-                tmp1 <- merge(tmp1, ddates, by.x=c('AID', 'FQ'), by.y=c("AID", "SAMPLE_ID"))
-                setorder(ddates, AID, -visit_dt)
-                tmp1 <- tmp1[, {
-                        z <- which(HXB2 == TRUE)[1];
-                        z <- ifelse(is.na(z), 1, z)
-                        list(FQ=FQ[z], visit_dt=visit_dt[z])
-                }, by='AID']
-
-
-                rows_to_del <- rownames(maf_mat)[which(grepl(paste0(tmp1$AID, collapse='|'), rownames(maf_mat) ))]
-                rows_to_del <- rows_to_del[! rows_to_del %in% tmp1$FQ]
-                
-                # Store -fq used so we can check exact sampling date
-                maf_mat <- maf_mat[! rownames(maf_mat) %in% rows_to_del, ]
-        }
-        filename=paste0('ptyr', pty_idx, '_basefreqs_used.csv')
-        write.csv(rownames(maf_mat), 
-                  file=file.path(dirname(files_pty$pat.path), filename))
-        rownames(maf_mat) <- gsub('-fq[0-9]$', '', rownames(maf_mat))
-        rm(tmp1, rows_to_del)
-
-        maf_file=paste0('ptyr', pty_idx, '_maf.csv')
-        maf_file=file.path(dirname(files_pty$pat.path), maf_file)
-        write.table(maf_mat, file=maf_file, quote=F, col.names=FALSE, sep=',')
-
-        cmd <- paste0('python ',args$TSI.dir,'/HIVPhyloTSI.py \\\n',
-                      ' -d ', args$TSI.dir, '/Model \\\n',
-                      ' -p ', files_pty$pat.path,' \\\n',
-                      ' -m ', files_pty$maf.path,' \\\n',
-                      ' -o ', files_pty$tsi.path, '\n'
-                     )
-        dfiles[pty==pty_idx, CMD:=cmd]
-
-        # guess I may need to free up some memory here
-        rm(maf_mat, ph.input, patstats)
-        gc()
-}
-
 
 ##################################
 # submit jobs 
